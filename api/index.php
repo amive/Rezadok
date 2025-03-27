96f037508a7228a85c668041d79b5456168988ad
amine <62792200+amive@users.noreply.github.com>
2025-03-27 07:40:31 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -57,26 +57,5 @@
     <button onclick="showForm('main')">رجوع</button>
 </div>
 
-<script>
-    function showForm(formType) {
-        document.getElementById("main-section").style.display = "none";
-        document.getElementById("login-form").style.display = "none";
-        document.getElementById("register-form").style.display = "none";
-
-        if (formType === "login") {
-            document.getElementById("login-form").style.display = "block";
-        } else if (formType === "register") {
-            document.getElementById("register-form").style.display = "block";
-        } else {
-            document.getElementById("main-section").style.display = "block";
-        }
-    }
-
-    function toggleDoctorFields() {
-        document.getElementById("doctorFields").style.display = 
-            (document.getElementById("role").value == "doctor") ? "block" : "none";
-    }
-</script>
-
 </body>
 </html>

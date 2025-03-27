a861bca9d47f04a2c64021e82fcbced074691c44
amine <62792200+amive@users.noreply.github.com>
2025-03-27 07:41:18 +0100

    Update index.js

--- public/script/index.js
+++ public/script/index.js
@@ -1,12 +1,18 @@
-function showForm(formType) {
-  document.getElementById("main-section").style.display = "none";
-  document.getElementById("login-form").style.display =
-    formType === "login" ? "block" : "none";
-  document.getElementById("register-form").style.display =
-    formType === "register" ? "block" : "none";
-}
+    function showForm(formType) {
+        document.getElementById("main-section").style.display = "none";
+        document.getElementById("login-form").style.display = "none";
+        document.getElementById("register-form").style.display = "none";
 
-function toggleDoctorFields() {
-  document.getElementById("doctorFields").style.display =
-    document.getElementById("role").value == "doctor" ? "block" : "none";
-}
+        if (formType === "login") {
+            document.getElementById("login-form").style.display = "block";
+        } else if (formType === "register") {
+            document.getElementById("register-form").style.display = "block";
+        } else {
+            document.getElementById("main-section").style.display = "block";
+        }
+    }
+
+    function toggleDoctorFields() {
+        document.getElementById("doctorFields").style.display = 
+            (document.getElementById("role").value == "doctor") ? "block" : "none";
+    }

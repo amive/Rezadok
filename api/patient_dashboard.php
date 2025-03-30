ee52b5317b47ca19b6b994c556d5db2084cf5d16
garahbilal <garahbilal02@gmail.com>
2025-03-30 02:53:41 +0100

    Update patient_dashboard.php

--- api/patient_dashboard.php
+++ api/patient_dashboard.php
@@ -10,30 +10,35 @@
 <body>
     <header>
         <h2><i class="fa-solid fa-stethoscope"></i> Rezadok</h2>
-        <nav>
             <!--الرةابط-->
-            <a href="#" class="icon-btn" data-text="الرسائل">
+        <nav>
+            <a href="patient_dashboard.php" class="icon-btn"data-text="الرئيسية">
+                <i class="fa-solid fa-house"></i>
+            </a>
+            <a href="discussions.php" class="icon-btn" data-text="الرسائل">
                 <i class="fa-solid fa-comments"></i>
             </a>
-            <a href="#" class="icon-btn" data-text="مواعيدي">
+            <a href="appointments.php" class="icon-btn" data-text="مواعيدي">
                 <i class="fa-solid fa-calendar-days"></i>
             </a>
-            <a href="#" class="icon-btn" data-text="البحث عن طبيب">
-                <i class="fa-solid fa-magnifying-glass"></i>
+            
+            <a href="#" class="icon-btn" data-text="إضافة منشور">
+                <i class="fa-solid fa-plus"></i>
             </a>
+            <!-- القائمة -->
             <div class="dropdown">
                 <button>
-                    <i class="fa-solid fa-user-circle"></i> <?php echo $_SESSION['user_name']; ?>
+                    <i class="fa-solid fa-user-circle"></i>
                 </button>
                 <div class="dropdown-content">
                     <a href="#"><i class="fa-solid fa-user"></i> حسابي</a>
-                    <a href="#"><i class="fa-solid fa-gear"></i> الإعدادات</a>
-                    <a href="#"><i class="fa-solid fa-right-from-bracket"></i> تسجيل الخروج</a>
+                    <a href="#"><i class="fa-solid fa-cog"></i> الإعدادات</a>
+                    <a href="#"><i class="fa-solid fa-sign-out-alt"></i> تسجيل الخروج</a>
                 </div>
             </div>
         </nav>
     </header>
     <!-- المنشورات -->
-    <h2> المنشورات <i class="fa-solid fa-newspaper" style="color: #63E6BE;"></i></h2>
+    <h2 id="h2-post"> المنشورات <i class="fa-solid fa-newspaper" style="color: #63E6BE;"></i></h2>
 </body>
 </html>

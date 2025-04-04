ed49a4e85c551a0b168b5faab4589184c63df937
amine <aminegrerifa20052@gmail.com>
2025-04-04 18:37:53 +0100

    db

--- api/db.php
+++ api/db.php
@@ -1,10 +1,10 @@
 <?php
 $host = "dpg-cvo0rt24d50c739at8ig-a.frankfurt-postgres.render.com";
-$username = "rezadok_user";
-$password = "QSuIUPpwpqpCW5wZ3OxibvwbWL3qWEsQ";
+$user = "rezadok_user";
+$pass = "QSuIUPpwpqpCW5wZ3OxibvwbWL3qWEsQ";
 $dbname = "rezadok";
 
-$conn = new mysqli($host, $username, $password, $dbname);
+$conn = new mysqli($host, $usern, $pass, $dbname);
 
 if ($conn->connect_error) {
     die("فشل الاتصال بقاعدة البيانات: " . $conn->connect_error);

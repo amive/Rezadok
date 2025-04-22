1297a824e05a86954c49c7a96a1e237456fd8076
amine <aminegrerifa20052@gmail.com>
2025-04-22 23:41:58 +0100

    aha

--- api/config.php
+++ api/config.php
@@ -1,12 +1,13 @@
 <?php
-$host = 'localhost'; // السيرفر المحلي
-$dbname = 'reza'; // اسم قاعدة البيانات
-$username = 'root'; // اسم المستخدم الافتراضي لـ XAMPP أو WAMP
-$password = ''; // كلمة المرور الافتراضية (فارغة في XAMPP)
+$host = getenv('DB_HOST'); // Replace with your environment variable for the host
+$dbname = getenv('DB_NAME'); // Replace with your environment variable for the database name
+$username = getenv('DB_USER'); // Replace with your environment variable for the username
+$password = getenv('DB_PASS'); // Replace with your environment variable for the password
+$port = getenv('DB_PORT'); // Replace with your environment variable for the port
 
 // إنشاء اتصال باستخدام PDO
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("فشل الاتصال بقاعدة البيانات: " . $e->getMessage());

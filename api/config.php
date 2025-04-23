3d44be27dd458a3a2bfa855e40bbec236ba3f1e3
amine <aminegrerifa20052@gmail.com>
2025-04-23 15:03:47 +0100

    ffs

--- api/config.php
+++ api/config.php
@@ -1,9 +1,9 @@
 <?php
-$host = $_ENV['DB_HOST'] ?? 'sql7.freesqldatabase.com'; // Default to localhost if not set
-$dbname = $_ENV['DB_NAME'] ?? 'sql7774920'; // Default to 'reza' if not set
-$username = $_ENV['DB_USER'] ?? 'sql7774920'; // Default to 'root' if not set
-$password = $_ENV['DB_PASS'] ?? '7vfTx9dfIL'; // Default to empty password if not set
-$port = $_ENV['DB_PORT'] ?? '3306'; // Default to MySQL port 3306 if not set
+$host = $_ENV['DB_HOST'] ; // Default to localhost if not set
+$dbname = $_ENV['DB_NAME'] ; // Default to 'reza' if not set
+$username = $_ENV['DB_USER']; // Default to 'root' if not set
+$password = $_ENV['DB_PASS'] ; // Default to empty password if not set
+$port = $_ENV['DB_PORT']; // Default to MySQL port 3306 if not set
 
 // إنشاء اتصال باستخدام PDO
 try {

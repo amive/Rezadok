a1e8f0d902f6ec47263c111bf45b2c94e8aa5734
garahbilal <garahbilal02@gmail.com>
2025-05-06 15:52:04 +0100

    Update book_appointments.php

--- api/book_appointments.php
+++ api/book_appointments.php
@@ -52,6 +52,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <head>
         <meta charset="UTF-8">
         <title>حجز موعد - Rezadok</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="Design/book_appointments.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     </head>

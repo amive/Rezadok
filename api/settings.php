2250ded1ee982b39f3ef8f8b5453fceb22954a13
garahbilal <garahbilal02@gmail.com>
2025-05-06 16:19:43 +0100

    Update settings.php

--- api/settings.php
+++ api/settings.php
@@ -51,8 +51,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html lang="ar">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Design/index.css">
+    <link rel="stylesheet" href="Design/settings.css">
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
     <title>إعدادات الحساب</title>
 </head>
@@ -77,6 +78,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <br>
         <button type="submit">تحديث البيانات</button>
     </form>
-    <a href="logout">تسجيل الخروج</a>
+    <footer>
+        <div class="reza"> © 2025 جميع الحقوق محفوظة لـ<strong>Rezadok</strong></div>  
+        <a href="https://www.github.com/amive" target="_blank" style="color: #390071;"><i class="fab fa-github" style="color: #390071;"></i>Amine</a> |
+        <a href="https://www.github.com/bilalgarah" target="_blank" style="color: #a52a2a;"><i class="fab fa-github"style="color: #a52a2a;"></i>Bilal</a>
+    </footer>
 </body>
 </html>
